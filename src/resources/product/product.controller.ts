b2262c9c59609fcd6dcd6bff2acf032b4260e9d8
AlaminMJ <86642366+AlaminMJ@users.noreply.github.com>
2023-02-12 17:23:34 +0600

    1 file modified, 1 added

--- src/resources/product/product.controller.ts
+++ src/resources/product/product.controller.ts
@@ -2,37 +2,34 @@ import validation from '@/middleware/validation';
 import HttpException from '@/utils/exceptions/http.exception';
 import { Controller } from '@/utils/interface/controller.interface';
 import { NextFunction, Router, Request, Response } from 'express';
+import Product from './product.interface';
 import ProductService from './product.service';
 import productSchema from './product.validation';
 
 class ProductController implements Controller {
-    public path = '/products';
-    public router = Router();
-    private Product = new ProductService();
+  public path = '/products';
+  public router = Router();
+  private Product = new ProductService();
 
-    constructor() {
-        this.initializeRouter();
-    }
-    private initializeRouter(): void {
-        this.router.post(
-            `${this.path}`,
-            validation(productSchema),
-            this.create
-        );
-    }
+  constructor() {
+    this.initializeRouter();
+  }
+  private initializeRouter(): void {
+    this.router.post(`${this.path}`, validation(productSchema), this.create);
+  }
 
-    private async create(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void> {
-        try {
-            console.log(this.Product);
-            res.status(201).json(req.body);
-        } catch (error: any) {
-            next(new HttpException(400, error.message));
-        }
+  private async create(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void | Product> {
+    try {
+      console.log(this.Product);
+      res.status(201).json(req.body);
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
     }
+  }
 }
 
 export default ProductController;
