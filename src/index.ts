c361947276dc1ad160a41ec186594fa0dcd4c7f0
AlaminMJ <alamin.mj.3228@gmail.com>
2023-02-16 21:30:44 +0600

    OK

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,20 @@ import App from './app';
 import 'dotenv/config';
 import 'module-alias/register';
 import validateEnv from '@/utils/validateEnv';
-import PostController from '@/resources/post/post.controller';
 import ProductController from './resources/product/product.controller';
-import ReturnColtroller from './resources/return/return.controller';
-import PruchaseColtroller from './resources/purchase/purchase.controller';
+import ReturnController from './resources/return/return.controller';
+import PurchaseController from './resources/purchase/purchase.controller';
 import UserController from './resources/user/user.controller';
 import SellController from './resources/sell/sell.controller';
 validateEnv();
 const app = new App(
-  [
-    new PostController(),
-    new ProductController(),
-    new ReturnColtroller(),
-    new PruchaseColtroller(),
-    new UserController(),
-    new SellController(),
-  ],
-  Number(process.env.PORT)
+    [
+        new ProductController(),
+        new ReturnController(),
+        new PurchaseController(),
+        new UserController(),
+        new SellController(),
+    ],
+    Number(process.env.PORT)
 );
 app.listen();
