74b9ad8d0c3bb7e3f7d48bb1769fc48824c97cfb
AlaminMJ <alamin.mj.3228@gmail.com>
2023-02-16 23:02:37 +0600

    after all I fixed the bug

--- src/resources/product/product.controller.ts
+++ src/resources/product/product.controller.ts
@@ -7,30 +7,34 @@ import ProductService, { ProductInterface } from './product.service';
 import productSchema from './product.validation';
 
 class ProductController implements Controller {
-  public path = '/products';
-  public router = Router();
-  private Product: ProductInterface;
+    public path = '/products';
+    public router = Router();
+    private Product = new ProductService();
 
-  constructor() {
-    this.initializeRouter();
-    this.Product = new ProductService();
-  }
-  private initializeRouter(): void {
-    this.router.post(`${this.path}`, validation(productSchema), this.create);
-  }
-
-  private async create(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<void | Product> {
-    try {
-      console.log(this.Product);
-      res.status(201).json(req.body);
-    } catch (error: any) {
-      next(new HttpException(400, error.message));
+    constructor() {
+        this.initializeRouter();
     }
-  }
+    private initializeRouter(): void {
+        this.router.post(
+            `${this.path}`,
+            validation(productSchema),
+            this.create
+        );
+    }
+
+    private create = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void | Product> => {
+        try {
+            const { name, unit, img_url } = req.body;
+            const result = await this.Product.create(name, unit, img_url);
+            res.status(201).json(result);
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
 }
 
 export default ProductController;
