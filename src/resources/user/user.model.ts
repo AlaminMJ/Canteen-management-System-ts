a836a8fab619aacad02a4421e7b429cfd2a59750
AlaminMJ <86642366+AlaminMJ@users.noreply.github.com>
2023-02-16 18:30:03 +0600

    1 file modified

--- src/resources/user/user.model.ts
+++ src/resources/user/user.model.ts
@@ -22,7 +22,6 @@ userSchema.pre<User>('save', async function (next) {
   if (!this.isModified('password')) {
     return next();
   }
-
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
